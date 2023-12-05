10c34149b59cd37dcc6b8812fea4210fa6aaa6f6
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2023-12-05 15:29:30 -0600

    back end 2 demo

---
+++ server/controller.js
@@ -0,0 +1,39 @@
+const movies = require('./db.json')
+let globalID = 11
+
+module.exports = {
+    getMovies: (req, res) => {
+        res.status(200).send(movies)
+    },
+    deleteMovie: (req, res) => {
+        let index = movies.findIndex(elem => elem.id === +req.params.id)
+        movies.splice(index, 1)
+        res.status(200).send(movies)
+    },
+    createMovie: (req, res) => {
+        const {title, rating, imageURL} = req.body
+
+        let newMovie = {
+            id: globalID, 
+            title: title,
+            rating: +rating,
+            imageURL //same as doing imageURL: imageURL
+        }
+        movies.push(newMovie)
+        globalID++
+        res.status(200).send(movies)
+    },
+    updateMovie: (req, res) => {
+        const {type} = req.body
+        let index = movies.findIndex(elem => elem.id === +req.params.id)
+        if(type === 'minus' && movies[index].rating >1) {
+            movies[index].rating -= 1
+            res.status(200).send(movies)
+        } else if (type === 'plus' && movies[index].rating <5){
+            movies[index].rating +=1
+            res.status(200).send(movies)
+        } else {
+            res.status(400).send('you got dose starz wromg. 1-5 homie. 1. to. 5.')
+        }
+    }
+}
